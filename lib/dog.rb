0e126a92b51aa80ddcf160bbaeba414b7a4bf3e0
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-15 17:49:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
+class Dog
+  def instance(name, breed)
+    @name = name
+    @breed = breed
+  end
+  def name=(name)
+    @name = name
+  end
 
+  def breed
+    @breed = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def name
+    @name
+  end
+
+end
